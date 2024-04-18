187821d1e2e23256326f821e6e5ffcee84d659bc
Ahria <ahrianicholas@gmail.com>
2024-04-18 03:16:59 -0700

    setup title screen

---
+++ scenes/TitleScreen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var main_game = preload("res://scenes/game.tscn")
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_packed(main_game)
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+func _on_options_button_pressed():
+	# Place holder for options menu
+
+	pass # Replace with function body.
