ffcc12f3d50ec619728d4b292fbc8774e9780082
Ahria Nicholas <43478751+Starryahri@users.noreply.github.com>
2024-04-20 18:28:46 -0700

    fix so global volumes carry across and update ui

--- src/options_menu.gd
+++ src/options_menu.gd
@@ -12,15 +12,7 @@ func _on_close_pressed():
 
 func _on_options_pressed():
 	self.show()
+	self.get_child(0).get_child(0).get_child(0).get_child(0).get_child(1).value = Global.music_volume
+	self.get_child(0).get_child(0).get_child(0).get_child(1).get_child(1).value = Global.sound_volume
 	options_open.emit()
-	pass # Replace with function body.
-
-
-func _on_sound_slider_value_changed(value):
-	Global.sound_volume = value
-	pass # Replace with function body.
-
-
-func _on_music_slider_value_changed(value):
-	Global.music_volume = value
 	pass # Replace with function body.
